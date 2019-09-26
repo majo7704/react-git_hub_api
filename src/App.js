03c344696f8533230b9f351e8bf4992ada753bce
j m <jm@js-macbook-pro.home>
2019-09-26 20:09:49 +0200

    search functionality works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Navbar from './components/layout/Navbar'
+import Users from './components/users/Users' //replaced UserItem
+import Search from './components/users/Search'
+
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  }
+  // async componentDidMount() {
+  //   //console.log(process.env.REACT_APP_GITHUB_CLIENT_ID)
+  //   //change state 1.
+  //   this.setState({loading: true})
+  //   //request to the github api 2.
+  //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+  //   //after the request we want our data to load and loading be false 3.
+  //   this.setState({users: res.data, loading: false})
+  // }
+//Search Github users
+  searchUsers = async text => {
+    this.setState({loading: true})
+    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+    //after the request we want our data to load and loading be false 3.
+    this.setState({ users: res.data.items, loading: false })
+  }
+  //Clear users from state
+  clearUsers = () => this.setState({users: [], loading: false})
+
+  render() {
+    return (
+      <div className="App">
+        <Navbar title="Github Finder" icon='fab fa-github' />
+        <div className='container'>
+          <Search searchUsers={this.searchUsers} clearUsers={this.clearUsers} showClear={this.state.users.length > 0 ? true : false}/>
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+
+
---
+++ src/components/users/UserItem.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function UserItem({ user: { avatar_url, login, html_url } }) { //const UserItem = () =>{}
+  //distractoring
+  //no more this because props are passed
+  return (
+    <div className='card text-center'>
+      <img src={avatar_url} alt="" className="round-img" style={{ width: '60px' }} />
+      <h3>{login}</h3>
+      <div>
+        <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
+      </div>
+    </div>
+  )
+}
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+}
